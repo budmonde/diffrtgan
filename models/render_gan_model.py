9ade92ef1f95d578477e94ec4edff1930417e39b
Budmonde Duinkharjav <budmonde@gmail.com>
2018-12-06 20:49:08 -0500

    Implement Plane RenderGAN

---
+++ models/render_gan_model.py
@@ -0,0 +1,106 @@
+import torch
+import itertools
+from util.image_pool import ImagePool
+from util.render_util import NormalizedRenderLayer
+from .base_model import BaseModel
+from . import networks
+
+
+class RenderGANModel(BaseModel):
+    def name(self):
+        return 'RenderGANModel'
+
+    @staticmethod
+    def modify_commandline_options(parser, is_train=True):
+        # default RenderGAN did not use dropout
+        parser.set_defaults(no_dropout=True)
+        return parser
+
+    def initialize(self, opt):
+        BaseModel.initialize(self, opt)
+
+        # specify the training losses you want to print out. The program will call base_model.get_current_losses
+        self.loss_names = ['D', 'G']
+        # specify the images you want to save/display. The program will call base_model.get_current_visuals
+        visual_names_tex = ['real_tex_A', 'real_tex_B', 'fake_tex_B']
+        visual_names_render = ['real_render_B', 'fake_render_B']
+        self.visual_names = visual_names_tex + visual_names_render
+
+        # specify the models you want to save to the disk. The program will call base_model.save_networks and base_model.load_networks
+        if self.isTrain:
+            self.model_names = ['G', 'D']
+        else:  # during test time, only load Gs
+            self.model_names = ['G']
+
+        # load/define networks
+        self.netG = networks.define_G(opt.input_nc, opt.output_nc, opt.ngf, opt.netG, opt.norm,
+                                        not opt.no_dropout, opt.init_type, opt.init_gain, self.gpu_ids)
+
+        # TODO make num samples configurable
+        self.render_layer = NormalizedRenderLayer(opt.fineSize, 4, self.device)
+
+        if self.isTrain:
+            use_sigmoid = opt.no_lsgan
+            self.netD = networks.define_D(opt.output_nc, opt.ndf, opt.netD,
+                                            opt.n_layers_D, opt.norm, use_sigmoid, opt.init_type, opt.init_gain, self.gpu_ids)
+
+        if self.isTrain:
+            self.fake_render_B_pool = ImagePool(opt.pool_size)
+            # define loss functions
+            self.criterionGAN = networks.GANLoss(use_lsgan=not opt.no_lsgan).to(self.device)
+            # initialize optimizers
+            self.optimizer_G = torch.optim.Adam(self.netG.parameters(), lr=opt.lr, betas=(opt.beta1, 0.999))
+            self.optimizer_D = torch.optim.Adam(self.netD.parameters(), lr=opt.lr, betas=(opt.beta1, 0.999))
+            self.optimizers = []
+            self.optimizers.append(self.optimizer_G)
+            self.optimizers.append(self.optimizer_D)
+
+    def set_input(self, input):
+        AtoB = self.opt.direction == 'AtoB'
+        self.real_tex_A = input['A' if AtoB else 'B'].to(self.device)
+        self.real_tex_B = input['B' if AtoB else 'A'].to(self.device)
+        self.real_render_B = self.render_layer(self.real_tex_B)
+
+        # TODO what is this?
+        #self.image_paths = input['A_paths' if AtoB else 'B_paths']
+
+    def forward(self):
+        self.fake_tex_B = self.netG(self.real_tex_A)
+        self.fake_render_B = self.render_layer(self.fake_tex_B)
+
+    def backward_D_basic(self, netD, real, fake):
+        # Real
+        pred_real = netD(real)
+        loss_D_real = self.criterionGAN(pred_real, True)
+        # Fake
+        pred_fake = netD(fake.detach())
+        loss_D_fake = self.criterionGAN(pred_fake, False)
+        # Combined loss
+        loss_D = (loss_D_real + loss_D_fake) * 0.5
+        # backward
+        loss_D.backward()
+        return loss_D
+
+    def backward_D(self):
+        fake_render_B = self.fake_render_B_pool.query(self.fake_render_B)
+        self.loss_D = self.backward_D_basic(
+                self.netD, self.real_render_B, fake_render_B)
+
+    def backward_G(self):
+        # GAN loss D(G(A))
+        self.loss_G = self.criterionGAN(self.netD(self.fake_render_B), True)
+        self.loss_G.backward()
+
+    def optimize_parameters(self):
+        # forward
+        self.forward()
+        # G
+        self.set_requires_grad([self.netD], False)
+        self.optimizer_G.zero_grad()
+        self.backward_G()
+        self.optimizer_G.step()
+        # D
+        self.set_requires_grad([self.netD], True)
+        self.optimizer_D.zero_grad()
+        self.backward_D()
+        self.optimizer_D.step()
---
+++ util/render_util.py
@@ -0,0 +1,190 @@
+import random
+import numpy as np
+
+import torch
+import torch.nn as nn
+
+import pyredner
+
+class PlaneRenderer(object):
+    def __init__(self, out_sz, num_samples, device):
+        super(PlaneRenderer, self).__init__()
+
+        self.camera = pyredner.Camera(
+            position     = torch.tensor([0.0, 0.0, -5.0]),
+            look_at      = torch.tensor([0.0, 0.0, 0.0]),
+            up           = torch.tensor([0.0, 1.0, 0.0]),
+            fov          = torch.tensor([45.0]),
+            clip_near    = 0.01,
+            resolution   = (out_sz, out_sz))
+
+        self.materials = [
+            pyredner.Material(diffuse_reflectance = torch.tensor(
+                [1.0, 1.0, 1.0], device=device)),
+            pyredner.Material(diffuse_reflectance = torch.tensor(
+                [0.0, 0.0, 0.0], device=device))
+            ]
+
+        self.shapes = [
+            pyredner.Shape(
+                torch.tensor([
+                    [-2.1, -2.1, 2.0],
+                    [-2.1, 2.1, 0.0],
+                    [2.1, -2.1, 2.0],
+                    [2.1, 2.1, 0.0]],
+                    device=device),
+                torch.tensor([
+                    [0, 1, 2],
+                    [1, 3, 2]],
+                    dtype = torch.int32,
+                    device=device),
+                torch.tensor([
+                    [1.0, 1.0],
+                    [1.0, 0.0],
+                    [0.0, 1.0],
+                    [0.0, 0.0]],
+                    device=device),
+                None,
+                0),
+            pyredner.Shape(
+                torch.tensor([
+                    [-1.0, -1.0, -7.0],
+                    [1.0, -1.0, -7.0],
+                    [-1.0, 1.0, -7.0],
+                    [1.0, 1.0, -7.0]],
+                    device=device),
+                torch.tensor([
+                    [0, 1, 2],
+                    [1, 3, 2]],
+                    dtype = torch.int32,
+                    device=device),
+                None,
+                None,
+                1)]
+
+        self.lights = [pyredner.Light(1, torch.tensor([40.0, 40.0, 40.0]))]
+
+        self.num_samples = num_samples
+
+    def __call__(self, sample):
+        # assumer sample is of type Tensor
+        new_material = pyredner.Material(diffuse_reflectance = sample)
+        self.materials[0] = new_material
+        scene = pyredner.Scene(
+            self.camera, self.shapes, self.materials, self.lights)
+        args = pyredner.RenderFunction.serialize_scene(
+            scene = scene,
+            num_samples = self.num_samples,
+            max_bounces = 1)
+        seed = random.randint(0, 255)
+        return pyredner.RenderFunction.apply(seed, *args)
+
+class RandomCrop(object):
+    def __init__(self, size):
+        super(RandomCrop, self).__init__()
+        self.size = size
+
+    def __call__(self, input):
+        y = np.random.choice(input.shape[0] - self.size)
+        x = np.random.choice(input.shape[1] - self.size)
+        return input[y : y + self.size, x : x + self.size, : ]
+
+class ToTensor(object):
+    def __init__(self):
+        super(ToTensor, self).__init__()
+        pass
+
+    def __call__(self, input):
+        return torch.FloatTensor(np.array(input))
+
+class HWC2CHW(object):
+    def __init__(self):
+        super(HWC2CHW, self).__init__()
+        pass
+
+    def __call__(self, input):
+        return input.transpose(1, 2).transpose(0, 1).contiguous()
+
+class CHW2HWC(object):
+    def __init__(self):
+        super(CHW2HWC, self).__init__()
+        pass
+
+    def __call__(self, input):
+        return input.transpose(0, 1).transpose(1, 2).contiguous()
+
+class Normalize(object):
+    def __init__(self, mean, std):
+        super(Normalize, self).__init__()
+        self.mean = mean
+        self.std = std
+
+    def __call__(self, input):
+        return (input - self.mean) / self.std
+
+class PlaneRenderLayer(nn.Module):
+    def __init__(self, out_sz, num_samples, device):
+        super(PlaneRenderLayer, self).__init__()
+        self.renderer = PlaneRenderer(out_sz, num_samples, device)
+
+    def forward(self, input):
+        out = self.renderer(input)
+        return out
+
+class HWC2CHWLayer(nn.Module):
+    def __init__(self):
+        super(HWC2CHWLayer, self).__init__()
+        pass
+
+    def forward(self, input):
+        return input.transpose(1, 2).transpose(0, 1).contiguous()
+
+class CHW2HWCLayer(nn.Module):
+    def __init__(self):
+        super(CHW2HWCLayer, self).__init__()
+        pass
+
+    def forward(self, input):
+        return input.transpose(0, 1).transpose(1, 2).contiguous()
+
+class NormalizeLayer(nn.Module):
+    def __init__(self, mean, std):
+        super(NormalizeLayer, self).__init__()
+        self.mean = mean
+        self.std = std
+
+    def forward(self, input):
+        return (input - self.mean) / self.std
+
+class StripBatchDimLayer(nn.Module):
+    def __init__(self):
+        super(StripBatchDimLayer, self).__init__()
+        pass
+
+    def forward(self, input):
+        return input[0]
+
+class AddBatchDimLayer(nn.Module):
+    def __init__(self):
+        super(AddBatchDimLayer, self).__init__()
+        pass
+
+    def forward(self, input):
+        return input.unsqueeze(0)
+
+class NormalizedRenderLayer(nn.Module):
+    def __init__(self, out_sz, num_samples, device):
+        super(NormalizedRenderLayer, self).__init__()
+        self.model = nn.Sequential(
+                StripBatchDimLayer(),
+                NormalizeLayer(-1.0, 2.0),
+                CHW2HWCLayer(),
+                PlaneRenderLayer(out_sz, num_samples, device),
+                HWC2CHWLayer(),
+                NormalizeLayer(0.5, 0.5),
+                AddBatchDimLayer(),
+        )
+
+    def forward(self, input):
+        out = self.model.forward(input)
+        return out
