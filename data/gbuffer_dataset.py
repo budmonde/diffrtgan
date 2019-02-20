59bca4b35d61172d12466d91193268cce3f373dd
Budmonde Duinkharjav <budmonde@gmail.com>
2019-02-20 16:16:16 -0500

    Add Network with Gbuffer outputs as priors

---
+++ data/gbuffer_dataset.py
@@ -0,0 +1,88 @@
+import random
+import re
+import os.path
+
+import numpy as np
+import torch
+
+from data.base_dataset import BaseDataset, get_transform
+from data.image_folder import make_dataset
+from util.image_util import imread
+
+
+class GbufferDataset(BaseDataset):
+    @staticmethod
+    def modify_commandline_options(parser, is_train):
+        return parser
+
+    def initialize(self, opt):
+        self.opt = opt
+        self.root = opt.dataroot
+
+        self.dir_img = os.path.join(opt.dataroot, 'img')
+        self.img_paths = make_dataset(self.dir_img)
+        self.img_paths = sorted(self.img_paths)
+        self.img_size = len(self.img_paths)
+
+        self.dir_mask = os.path.join(opt.dataroot, 'mask')
+        self.mask_paths = make_dataset(self.dir_mask)
+        self.mask_paths = sorted(self.mask_paths)
+        self.mask_size = len(self.mask_paths)
+
+        self.dir_position = os.path.join(opt.dataroot, 'position')
+        self.position_paths = make_dataset(self.dir_position)
+        self.position_paths = sorted(self.position_paths)
+        self.position_size = len(self.position_paths)
+
+        self.dir_normal = os.path.join(opt.dataroot, 'normal')
+        self.normal_paths = make_dataset(self.dir_normal)
+        self.normal_paths = sorted(self.normal_paths)
+        self.normal_size = len(self.normal_paths)
+
+        assert(self.img_size == self.mask_size)
+        assert(self.mask_size == self.position_size)
+        assert(self.position_size == self.normal_size)
+
+        self.transform = get_transform(opt)
+
+    def __getitem__(self, index):
+        B_img_path = self.img_paths[index % self.img_size]
+        B_mask_path = self.mask_paths[index % self.mask_size]
+        A_position_path = self.position_paths[index % self.position_size]
+        A_normal_path = self.normal_paths[index % self.normal_size]
+
+        # Setup A
+        A_position = imread(A_position_path)
+        A_normal = imread(A_normal_path)
+
+        A_position = self.transform(A_position)
+        A_normal = self.transform(A_normal)
+
+        A = torch.cat([A_position, A_normal], dim=0)
+
+        # Setup B
+        B_img = imread(B_img_path)
+        B_mask = imread(B_mask_path)
+
+        B = B_img * B_mask
+        B = self.transform(B)
+
+        # Setup B_paths, B_name
+        B_path = B_img_path
+        B_name = re.split('/|\.', B_img_path)[-2]
+
+        if self.opt.input_nc == 1:  # RGB to gray
+            tmp = A[0, ...] * 0.299 + A[1, ...] * 0.587 + A[2, ...] * 0.114
+            A = tmp.unsqueeze(0)
+
+        if self.opt.output_nc == 1:  # RGB to gray
+            tmp = B[0, ...] * 0.299 + B[1, ...] * 0.587 + B[2, ...] * 0.114
+            B = tmp.unsqueeze(0)
+        return {'A': A, 'B': B,
+                'B_paths': B_path, 'B_name': B_name}
+
+    def __len__(self):
+        return self.img_size
+
+    def name(self):
+        return 'GbufferDataset'
---
+++ models/gbuffer_model.py
@@ -0,0 +1,162 @@
+import itertools
+import json
+
+import numpy as np
+import torch
+
+from . import networks
+from .base_model import BaseModel
+from util.image_pool import ImagePool
+from util.image_util import imread
+from util.render_util import RenderConfig
+from util.torch_util import NormalizedRenderLayer, NormalizedCompositLayer
+
+
+class GbufferModel(BaseModel):
+    def name(self):
+        return 'GbufferModel'
+
+    @staticmethod
+    def modify_commandline_options(parser, is_train=True):
+        parser.set_defaults(
+                pool_size=0,
+                no_flip=True,
+                no_dropout=True,
+                display_ncols=5,
+                input_nc=6,
+                loadSize=256,
+        )
+        # Render Config
+        parser.add_argument('--meshes_path', type=str, default='./datasets/meshes/one_mtl', help='Path of mesh pool to render')
+        parser.add_argument('--envmaps_path', type=str, default='./datasets/envmaps/rasters', help='Path of envmap pool to render')
+        parser.add_argument('--config_path', type=str, default='./datasets/empty.json', help='Path to camera configs for target images')
+        parser.add_argument('--mc_subsampling', type=int, default=4, help='Number of Monte-Carlo subsamples per-pixel on rendering step')
+        parser.add_argument('--mc_max_bounces', type=int, default=2, help='Max number of Monte-Carlo bounces ray on rendering step')
+
+        # Visuals Config
+        parser.add_argument('--viz_composit_bkgd_path', type=str, default='./datasets/textures/transparency/transparency.png', help='Compositing background used for visualization of semi-transparent textures')
+
+        # Network Config
+        parser.add_argument('--texture_nc', type=int, default=4, help='Number of channels in the texture output')
+
+        return parser
+
+    def initialize(self, opt):
+        BaseModel.initialize(self, opt)
+
+        # specify the training losses you want to print out. The program will call base_model.get_current_losses
+        self.loss_names = ['D', 'G']
+        # specify the images you want to save/display. The program will call base_model.get_current_visuals
+        visual_names_A = ['real_A_position', 'real_A_normal']
+        visual_names_B = ['fake_B_tex_show', 'real_B', 'fake_B']
+        self.visual_names = visual_names_A + visual_names_B
+
+        # specify the models you want to save to the disk. The program will call base_model.save_networks and base_model.load_networks
+        if self.isTrain:
+            self.model_names = ['G', 'D']
+        else:  # during test time, only load Gs
+            self.model_names = ['G']
+
+        # load/define networks
+        self.netG = networks.define_G(opt.input_nc, opt.texture_nc, opt.ngf, opt.netG, opt.norm,
+                                        not opt.no_dropout, opt.init_type, opt.init_gain, self.gpu_ids)
+
+        render_background = None
+        visdom_background = torch.tensor(imread(opt.viz_composit_bkgd_path), dtype=torch.float32)
+
+        self.render_config = RenderConfig(json.loads(open(opt.config_path).read()))
+
+        render_kwargs = {
+            "meshes_path": opt.meshes_path,
+            "envmaps_path": opt.envmaps_path,
+            "out_sz": opt.fineSize,
+            "num_samples": opt.mc_subsampling,
+            "max_bounces": opt.mc_max_bounces,
+            "device": self.device,
+            "logger": None,
+            "config": self.render_config,
+        }
+        composit_kwargs = {
+            "background": render_background,
+            "size": opt.fineSize,
+            "device": self.device,
+        }
+        self.render_layer = NormalizedRenderLayer(render_kwargs, composit_kwargs)
+
+        visdom_composit_kwargs = {
+            "background": render_background,
+            "size": opt.fineSize,
+            "device": self.device,
+        }
+        self.composit_layer = NormalizedCompositLayer(**visdom_composit_kwargs)
+
+        if self.isTrain:
+            use_sigmoid = opt.no_lsgan
+            self.netD = networks.define_D(opt.output_nc, opt.ndf, opt.netD,
+                                            opt.n_layers_D, opt.norm, use_sigmoid, opt.init_type, opt.init_gain, self.gpu_ids)
+
+        if self.isTrain:
+            self.fake_B_pool = ImagePool(opt.pool_size)
+            # define loss functions
+            self.criterionGAN = networks.GANLoss(use_lsgan=not opt.no_lsgan).to(self.device)
+            # initialize optimizers
+            self.optimizer_G = torch.optim.Adam(self.netG.parameters(), lr=opt.lr, betas=(opt.beta1, 0.999))
+            self.optimizer_D = torch.optim.Adam(self.netD.parameters(), lr=opt.lr, betas=(opt.beta1, 0.999))
+            self.optimizers = []
+            self.optimizers.append(self.optimizer_G)
+            self.optimizers.append(self.optimizer_D)
+
+    def set_input(self, input):
+        self.real_A = input['A'].to(self.device)
+        self.real_B = input['B'].to(self.device)
+        self.real_B_name = input['B_name']
+
+        # Gbuffer visuals
+        self.real_A_position = self.real_A[:,:3,:,:]
+        self.real_A_normal = self.real_A[:,3:,:,:]
+
+    def forward(self):
+        self.fake_B_tex = self.netG(self.real_A)
+        # Set camera parameters and pass to renderer
+        self.render_config.set_config(self.real_B_name[0])
+        self.fake_B = self.render_layer(self.fake_B_tex)
+
+        # Composit for visuals
+        self.fake_B_tex_show = self.composit_layer(self.fake_B_tex)
+
+    def backward_D_basic(self, netD, real, fake):
+        # Real
+        pred_real = netD(real)
+        loss_D_real = self.criterionGAN(pred_real, True)
+        # Fake
+        pred_fake = netD(fake.detach())
+        loss_D_fake = self.criterionGAN(pred_fake, False)
+        # Combined loss
+        loss_D = (loss_D_real + loss_D_fake) * 0.5
+        # backward
+        loss_D.backward()
+        return loss_D
+
+    def backward_D(self):
+        fake_B = self.fake_B_pool.query(self.fake_B)
+        self.loss_D = self.backward_D_basic(
+                self.netD, self.real_B, fake_B)
+
+    def backward_G(self):
+        # GAN loss D(G(A))
+        self.loss_G = self.criterionGAN(self.netD(self.fake_B), True)
+        self.loss_G.backward()
+
+    def optimize_parameters(self):
+        # forward
+        self.forward()
+        # G
+        self.set_requires_grad([self.netD], False)
+        self.optimizer_G.zero_grad()
+        self.backward_G()
+        self.optimizer_G.step()
+        # D
+        self.set_requires_grad([self.netD], True)
+        self.optimizer_D.zero_grad()
+        self.backward_D()
+        self.optimizer_D.step()
