Fix Gaussian Noise Bug to broadcast over whole image

models/proto_model.py
@@ -10,7 +10,7 @@ from .base_model import BaseModel
 from util.image_pool import ImagePool
 from util.image_util import imread
 from util.render_util import RenderConfig
-from util.torch_util import NormalizedRenderLayer, NormalizedCompositLayer, GaussianNoiseLayer
+from util.torch_util import NormalizedRenderLayer, NormalizedCompositLayer
 
 
 class ProtoModel(BaseModel):
@@ -72,14 +72,20 @@ class ProtoModel(BaseModel):
             "device": self.device,
             "config": self.render_config,
         }
+        noise_kwargs = {
+            "sigma": opt.gaussian_sigma,
+            "device": self.device,
+        }
         composit_kwargs = {
             "background": np.array([[[0.0, 0.0, 0.0]]]),
             "size": opt.fineSize,
             "device": self.device,
         }
-        self.render_layer = NormalizedRenderLayer(render_kwargs, composit_kwargs)
-
-        self.noise_layer = GaussianNoiseLayer(self.device, sigma=0.5)
+        self.render_layer = NormalizedRenderLayer(
+            render_kwargs,
+            noise_kwargs,
+            composit_kwargs
+        )
 
         background = imread(opt.viz_composit_bkgd_path)
 
@@ -109,7 +115,6 @@ class ProtoModel(BaseModel):
     def set_input(self, input):
         self.real_A = input['A'].to(self.device)
         self.real_B = input['B'].to(self.device)
-        self.real_B = self.noise_layer(self.real_B)
         self.filename = input['filename']
 
     def forward(self):
@@ -117,7 +122,6 @@ class ProtoModel(BaseModel):
         # Set camera parameters and pass to renderer
         self.render_config.set_config(self.filename[0])
         self.fake_B = self.render_layer(self.fake_B_tex)
-        self.fake_B = self.noise_layer(self.fake_B)
 
         # Composit for visuals
         self.fake_B_tex_show = self.composit_layer(self.fake_B_tex)

util/torch_util.py
@@ -95,13 +95,14 @@ class NormalizedCompositLayer(nn.Module):
         return out
 
 class NormalizedRenderLayer(nn.Module):
-    def __init__(self, render_kwargs, composit_kwargs):
+    def __init__(self, render_kwargs, noise_kwargs, composit_kwargs):
         super(NormalizedRenderLayer, self).__init__()
         self.model = nn.Sequential(
                 StripBatchDimLayer(),
                 NormalizeLayer(-1.0, 2.0),
                 CHW2HWCLayer(),
                 RenderLayer(**render_kwargs),
+                GaussianNoiseLayer(**noise_kwargs),
                 CompositLayer(**composit_kwargs),
                 HWC2CHWLayer(),
                 NormalizeLayer(0.5, 0.5),

util/transform_util.py
@@ -17,7 +17,9 @@ class Debug(object):
         return input
 
 class GaussianNoise(object):
-    def __init__(self, device, sigma=0.1):
+    # NOTE: Assumes input is HWC Formatted Image
+    #       If input has 4 channels, it will ignore the alpha channel
+    def __init__(self, sigma, device):
         super(GaussianNoise, self).__init__()
         self.sigma = sigma
         self.noise = torch.tensor(0.0, dtype=torch.float32, device=device)
@@ -25,9 +27,23 @@ class GaussianNoise(object):
     def __call__(self, input):
         if self.sigma != 0:
             scale = self.sigma * input.detach()
+            if input.size()[-1] == 4:
+                scale[:,:,-1] *= 0
             sampled_noise = self.noise.repeat(*input.size()).normal_() * scale
             input = input + sampled_noise
         return input
+
+class GaussianNoiseNP(object):
+    # TODO: For some reason this one looks a little brighter than the torch version(?)
+    def __init__(self, sigma):
+        super(GaussianNoiseNP, self).__init__()
+        self.sigma = sigma
+
+    def __call__(self, input):
+        scale = self.sigma * input
+        sampled_noise = np.random.normal(np.zeros(input.shape), 1.0) * scale
+        out = input + sampled_noise
+        return out
 
 class NP2PIL(object):
     def __init__(self):