0238d87d900b434621daadd0552ac0a79d7d67d9
Budmonde Duinkharjav <budmonde@gmail.com>
2019-04-11 22:43:03 -0400

    Add configurability to test render script

--- test_render.py
+++ test_render.py
@@ -1,22 +1,43 @@
 import torch
 
 from util.image_util import imread, imwrite
+from util.render_util import RenderConfig
 from util.torch_util import RenderLayer
 
 
-meshes_path = "./datasets/meshes/one_mtl"
+meshes_path = "./datasets/meshes/one"
 envmaps_path = "./datasets/envmaps/one"
-fineSize = 256
-num_samples = 4
-max_bounces = 2
+fineSize = 512
+num_samples = 200
+max_bounces = 1
 device = torch.device('cuda:0')
-opaque_path = "./datasets/textures/checkerboard/train/checkerboard.png"
-alpha_path = "./datasets/textures/decal/train/decal.png"
+opaque_path = "./datasets/textures/checkerboard.png"
+alpha_path = "./datasets/textures/decal.png"
+
+render_config = RenderConfig({
+    'test': {
+        'geo_mesh_path': './datasets/meshes/one/octavia_clean.obj',
+        'tex_envmap_path': './datasets/envmaps/one/sunsky.exr',
+        'geo_rotation': [1.5707963267948966, 0.15, 0.0],
+        'geo_translation': [0.0, -0.65, 0.0],
+        'geo_distance': 8.3,
+        'render_seed': 0,
+    }
+})
+render_config.set_config('test')
+render_kwargs = {
+    "meshes_path":  meshes_path,
+    "envmaps_path": envmaps_path,
+    "out_sz":       fineSize,
+    "num_samples":  num_samples,
+    "max_bounces":  max_bounces,
+    "device":       device,
+    "config":       render_config,
+}
+render_layer = RenderLayer(**render_kwargs)
 
 opaque = torch.tensor(imread(opaque_path), dtype=torch.float32, device=device)
 alpha = torch.tensor(imread(alpha_path), dtype=torch.float32, device=device)
-
-render_layer = RenderLayer(meshes_path, envmaps_path, fineSize, num_samples, max_bounces, device)
 
 out = render_layer(opaque)
 imwrite(out, "debug/test_render_opaque_out.png")
