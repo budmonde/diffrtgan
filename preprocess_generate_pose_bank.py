201ce6aa15030609f3523225a64a31ed2b45c992
Budmonde Duinkharjav <budmonde@gmail.com>
2019-04-16 20:36:09 -0400

    Update pose bank generator

--- preprocess_generate_pose_bank.py
+++ preprocess_generate_pose_bank.py
@@ -1,4 +1,5 @@
 import argparse
+import json
 import math
 import os
 
@@ -7,136 +8,140 @@ import torch
 import redner
 import pyredner
 
+from util.geometry_util import camera_parameters
 from util.image_util import imread, imwrite
+from util.misc_util import *
 
 
 # Argparser
 parser = argparse.ArgumentParser()
-parser.add_argument('--model_path', type=str, default='./datasets/meshes/one/octavia_clean.obj', help='Model path for mesh to render')
+parser.add_argument('--models_path', type=str, default='./datasets/meshes/one', help='Model path for mesh to render')
 parser.add_argument('--resolution', type=int, default=256, help='Resolution')
-parser.add_argument('--num_azimuth', type=int, default=100, help='Number of samples along azimuth parameter')
-parser.add_argument('--num_distance', type=int, default=10, help='Number of samples along distance parameter')
+parser.add_argument('--num_elev', type=int, default=50, help='Number of samples along azimuth parameter')
+parser.add_argument('--min_elev', type=float, default=0.0, help='Minimum elevation')
+parser.add_argument('--max_elev', type=float, default=math.pi/8.0, help='Maximum elevation')
 parser.add_argument('--gpu_id', type=int, default=0, help='GPU id to use')
 opt = parser.parse_args()
 
+
+def generate_poses(model_path, output_path):
+    # Init logger
+    log = dict()
+
+    # Load renderer configs
+    material_map, mesh_list, light_map = pyredner.load_obj(model_path)
+    material_id_map = {}
+    materials = []
+    count = 0
+    for key, value in material_map.items():
+        material_id_map[key] = count
+        count += 1
+        materials.append(value)
+
+    shapes = []
+    for mtl_name, mesh in mesh_list:
+        shapes.append(
+            pyredner.Shape(
+                vertices = mesh.vertices,
+                indices = mesh.indices,
+                uvs = mesh.uvs,
+                normals = mesh.normals,
+                material_id = material_id_map[mtl_name]
+            )
+        )
+
+    envmap = pyredner.EnvironmentMap(torch.tensor(
+        imread('./datasets/envmaps/one/sunsky.exr'),
+        dtype=torch.float32,
+        device=pyredner.get_device()))
+
+    # Object pose parameters
+    euler_angles = [0.0, 0.0, 0.0]
+    translation = [0.0, -0.75, 0.0]
+    up = [0.0, 1.0, 0.0]
+    distance = 7.0
+
+    # Setup base scene to modify during iterations
+    cam_params = camera_parameters(euler_angles, translation, distance, up)
+
+    camera = pyredner.Camera(
+            position = torch.tensor(cam_params[0], dtype=torch.float32),
+            look_at = torch.tensor(cam_params[1], dtype=torch.float32),
+            up = torch.tensor(cam_params[2], dtype=torch.float32),
+            fov = torch.tensor([45.0]),
+            clip_near = 1e-2,
+            resolution = (opt.resolution, opt.resolution),
+            fisheye = False)
+
+    scene = pyredner.Scene(camera, shapes, materials,
+            area_lights = [], envmap = envmap)
+
+    # Generate alphamasks
+    for i in range(opt.num_elev):
+        # Set elevation angle
+        elev_pc = i / opt.num_elev
+        elevation = opt.max_elev * elev_pc + opt.min_elev * (1 - elev_pc)
+        euler_angles[1] = elevation
+
+        # Calculate number of azimuthal iterations
+        num_azimuth = int(opt.num_elev * math.sin(math.pi/2 - elevation))
+        for j in range(num_azimuth):
+            # Set azimuthal angle
+            azimuth_pc = j / num_azimuth
+            azimuth = math.pi * 2 * azimuth_pc
+
+            euler_angles[0] = azimuth
+
+            print('Params: Elevation - {:.4f}\tAzimuth - {:.4f}'\
+                    .format(elevation, azimuth))
+
+            # Set Camera params
+            cam_params = camera_parameters(euler_angles, translation, distance, up)
+
+            # Update scene params
+            scene.camera = pyredner.Camera(
+                position = torch.tensor(cam_params[0], dtype=torch.float32),
+                look_at = torch.tensor(cam_params[1], dtype=torch.float32),
+                up = torch.tensor(cam_params[2], dtype=torch.float32),
+                fov = torch.tensor([45.0]),
+                clip_near = 1e-2,
+                resolution = (opt.resolution, opt.resolution),
+                fisheye = False
+            )
+            args = pyredner.RenderFunction.serialize_scene(
+                scene = scene,
+                num_samples = 1,
+                max_bounces = 1,
+                channels = [redner.channels.alpha])
+
+            out = pyredner.RenderFunction.apply(1, *args)
+
+            fn = gen_hash(6)
+            imwrite(out, os.path.join(output_path, '{}.png'.format(fn)))
+            log[fn] = {
+                'elevation': elevation,
+                'azimuth': azimuth
+            }
+    return log
 
 # Set Redner device
 device = torch.device('cuda:{}'.format(opt.gpu_id) if opt.gpu_id != -1 else 'cpu')
 pyredner.set_use_gpu(torch.cuda.is_available())
 pyredner.set_device(device)
 
+# Generate poses
+paths = get_child_paths(opt.models_path)
+paths = list(filter(lambda p: get_ext(p) == 'obj', paths))
 
-# Load renderer configs
-# TODO: Load list of meshes
-material_map, mesh_list, light_map = pyredner.load_obj(opt.model_path)
-material_id_map = {}
-materials = []
-count = 0
-for key, value in material_map.items():
-    material_id_map[key] = count
-    count += 1
-    materials.append(value)
+for path in paths:
+    print('Generating poses for {}'.format(path))
 
-shapes = []
-for mtl_name, mesh in mesh_list:
-    shapes.append(
-        pyredner.Shape(
-            vertices = mesh.vertices,
-            indices = mesh.indices,
-            uvs = mesh.uvs,
-            normals = mesh.normals,
-            material_id = material_id_map[mtl_name]
-        )
-    )
+    model_name = get_fn(path)
+    poses_dir = os.path.join('./datasets/poses', model_name)
 
-envmap = pyredner.EnvironmentMap(torch.tensor(
-    imread('./datasets/envmaps/one/sunsky.exr'),
-    dtype=torch.float32,
-    device=pyredner.get_device()))
+    if not os.path.exists(poses_dir):
+        os.makedirs(poses_dir)
 
-# TODO: move to geometry_utils
-def obj2cam(euler_angles, translation, distance, up):
-    # Calculate Camera Position
-    cam_azim = -euler_angles[0]
-    cam_elev = math.pi/2 - euler_angles[1]
-    cam_pos_hat = torch.tensor([
-        torch.cos(cam_azim)*torch.sin(cam_elev),
-        torch.cos(cam_elev),
-        torch.sin(cam_azim)*torch.sin(cam_elev)
-    ])
-    cam_position = cam_pos_hat * distance - translation
-
-    # Calculate Camera Look-at
-    cam_dir = -cam_pos_hat
-    cam_look_at = cam_position + cam_dir
-
-    # Calculate Camera Up direction
-    axis = -cam_pos_hat
-    cam_up = torch.dot(up, axis) * axis +\
-             torch.cross(axis, up) * torch.sin(euler_angles[2]) +\
-             torch.cross(torch.cross(axis, up), axis) * torch.cos(euler_angles[2])
-    return (cam_position, cam_look_at, cam_up)
-
-# Iteration variable
-euler_angles = torch.tensor([0.0, 0.0, 0.0])
-distance = torch.tensor([6.0])
-
-# Static
-translation = torch.tensor([0.0, -0.75, 0.0])
-up = torch.tensor([0.0, 1.0, 0.0])
-
-# Setup base scene to modify during iterations
-cam_params = obj2cam(euler_angles, translation, distance, up)
-
-camera = pyredner.Camera(
-        position = cam_params[0],
-        look_at = cam_params[1],
-        up = cam_params[2],
-        fov = torch.tensor([45.0]),
-        clip_near = 1e-2,
-        resolution = (opt.resolution, opt.resolution),
-        fisheye = False)
-
-scene = pyredner.Scene(
-        camera,
-        shapes,
-        materials,
-        area_lights = [],
-        envmap = envmap)
-
-model_name = opt.model_path.split('/')[-1].split('.')[0]
-poses_dir = os.path.join('./datasets/poses', model_name, '{}_{}'.format(opt.num_azimuth, opt.num_distance))
-
-if not os.path.exists(poses_dir):
-    os.makedirs(poses_dir)
-
-print("Generating {} poses".format(opt.num_azimuth * opt.num_distance))
-for i in range(opt.num_azimuth):
-    for j in range(opt.num_distance):
-        print("Params: azimuth - {}\tdistance - {}".format(i, j))
-
-        # Compute variables
-        euler_angles[0] = 2 * math.pi * i / opt.num_azimuth
-        distance = 6 + 4 * j / opt.num_distance
-
-        cam_params = obj2cam(euler_angles, translation, distance, up)
-
-        # Update scene params
-        scene.camera = pyredner.Camera(
-            position = cam_params[0],
-            look_at = cam_params[1],
-            up = cam_params[2],
-            fov = torch.tensor([45.0]),
-            clip_near = 1e-2,
-            resolution = (opt.resolution, opt.resolution),
-            fisheye = False
-        )
-        args = pyredner.RenderFunction.serialize_scene(
-            scene = scene,
-            num_samples = 1,
-            max_bounces = 1,
-            channels = [redner.channels.alpha])
-
-        out = pyredner.RenderFunction.apply(1, *args)
-
-        imwrite(out, os.path.join(poses_dir, '{}_{}.png'.format(i, j)))
+    log = generate_poses(path, poses_dir)
+    with open(os.path.join(poses_dir, 'data.json'), 'w') as f:
+        json.dump(log, f)
