2f7dd20c0fde4d1a0a03c5a18f18a8bb41a39d20
Budmonde Duinkharjav <budmonde@gmail.com>
2019-04-17 01:55:40 -0400

    Update pose estimation script

--- preprocess_estimate_pose.py
+++ preprocess_estimate_pose.py
@@ -4,50 +4,56 @@ import math
 import os
 
 import numpy as np
+import skimage
+import skimage.morphology
 
 from util.image_util import imread
-from util.transform_util import Resize
+from util.misc_util import *
 
 
 # Argparser
 parser = argparse.ArgumentParser()
 parser.add_argument('--dataroot', type=str, default='./datasets/cosy/', help='Path to dataset for pose estimation')
-parser.add_argument('--data_resolution', type=int, default=256, help='Resolution')
-parser.add_argument('--pose_bank_path', type=str, default='./datasets/poses/octavia_clean/100_10', help='Path for pose bank to use')
+parser.add_argument('--resolution', type=int, default=256, help='resolution')
+parser.add_argument('--pose_bank_path', type=str, default='./datasets/poses/octavia_clean', help='Path for pose bank to use')
 opt = parser.parse_args()
 
-# Get granularity of pose bank
-poses_dirname = opt.pose_bank_path.split('/')[-1]
-num_azimuth, num_distance = (int(i) for i in poses_dirname.split('_'))
+def crop_and_rescale(img):
+    img = img.astype('uint8')
+    props = skimage.measure.regionprops(img)
+    bbox = props[0].bbox
+    img = img[bbox[0]:bbox[3],bbox[1]:bbox[4],:]
+    img = skimage.transform.resize(img,
+        (opt.resolution, opt.resolution), order = 0)
+    return img
+
+# Load pose bank config
+with open(os.path.join(opt.pose_bank_path, 'data.json'), 'r') as f:
+    data = json.load(f)
 
 # Load images to estimate poses for
-masks_path = os.path.join(opt.dataroot, 'mask')
-fn_list = os.listdir(masks_path)
+fpath_list = get_child_paths(os.path.join(opt.dataroot, 'mask'))
 
 # Initialize objects before iteration
-resizer = Resize(opt.data_resolution, order = 0)
 best_pose_dict = dict()
 
-for fn in fn_list:
-    fpath = os.path.join(masks_path, fn)
+for fpath in fpath_list:
+    image = crop_and_rescale(imread(fpath))
 
-    image = resizer(imread(fpath)[:,:,0:1])
-    best_pose = (0, 0)
+    best_pose = dict()
     best_loss = float("inf")
 
-    key = fn.split('.')[0]
-    print("Processing {}".format(key), end='\t')
-    for i in range(num_azimuth):
-        for j in range(num_distance):
-            pose_path = os.path.join(opt.pose_bank_path, '{}_{}.png'.format(i, j))
-            pose_img = imread(pose_path)[:,:,0:1]
+    key = get_fn(fpath)
 
-            l2 = np.sum((image - pose_img)**2)
-            if l2 < best_loss:
-                azimuth = 2 * math.pi * i / num_azimuth
-                distance = 6 + 4 * j / num_distance
-                best_pose = (azimuth, distance)
-                best_loss = l2
+    print("Processing {}".format(key), end='\t')
+    for k, v in data.items():
+        pose_path = os.path.join(opt.pose_bank_path, '{}.png'.format(k))
+        pose_img = crop_and_rescale(imread(pose_path)[:,:,0:1])
+
+        l2 = np.sum((image - pose_img)**2)
+        if l2 < best_loss:
+            best_pose['geo_rotation'] = [v['azimuth'], v['elevation'], 0.0]
+            best_loss = l2
 
     print("Best pose: {}".format(best_pose))
     best_pose_dict[key] = best_pose
