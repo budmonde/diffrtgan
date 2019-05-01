5a594b3ff2b8dce63c37fa1a62a077642ab0b418
Budmonde Duinkharjav <budmonde@gmail.com>
2019-05-01 15:37:26 -0400

    Add rotation feature to blender script

--- blender/add_dirt_main.py
+++ blender/add_dirt_main.py
@@ -3,6 +3,9 @@ import json
 import os
 import subprocess
 
+from skimage.transform import rotate
+
+from util.image_util import imread, imwrite
 from util.misc_util import *
 from util.sample_util import *
 
@@ -17,19 +20,26 @@ if not os.path.exists(OUT_PATH):
 sampler = RGBFileSamplerFactory('./datasets/distributions/diffuse.txt')
 
 data = dict()
-for path in get_child_paths(OBJ_DIR, ext='obj'):
-    data[path] = sampler()
+for in_path in get_child_paths(OBJ_DIR, ext='obj'):
+    mesh_name = get_fn(path)
+    out_path = os.path.join(OUT_PATH, f'{mesh_name}.png')
+    data[mesh_name] = sampler()
     subprocess.run([
         BLENDER,
             '--background',
             '--python', 'blender/add_dirt_blender.py',
             '--',
-                '--input_path', path,
-                '--output_path', OUT_PATH,
+                '--input_path', in_path,
+                '--output_path', out_path,
                 '--albedo_r', str(data[path][0]),
                 '--albedo_g', str(data[path][1]),
                 '--albedo_b', str(data[path][2])],
         check=True)
+
+    # Post process output
+    image = imread(out_path)
+    image = rotate(image, 180)
+    imwrite(image, out_path)
 
     with open(os.path.join(OUT_PATH, 'data.json'), 'w') as f:
         json.dump(data, f)
